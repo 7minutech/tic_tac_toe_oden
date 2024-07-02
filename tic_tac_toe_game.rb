62622b55acb3b1607480af2e0aa96c595d10662e
7minutech <elijohnson1107@gmail.com>
2024-07-02 17:30:23 -0500

    Changed class formatting
    Added spaces and a comment at the top along
    with attr_accessor for the instance variables

--- tic_tac_toe_game.rb
+++ tic_tac_toe_game.rb
@@ -1,19 +1,29 @@
+# class that sets up a game of tic tac toe
 class TicTacToe
+  attr_accessor :board, :current_move
+
   def initialize
     @borad = []
     @current_move = ""
   end
+
   def fill_board
   end
+
   def display_board
   end
+
   def get_player_move
   end
+
   def place_move(move)
   end
+
   def winner?
   end
+
   def play_game
   end
+
 end
 
