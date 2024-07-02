5015e813fe6f84318a2c2dd4bf6e770788db6c02
7minutech <elijohnson1107@gmail.com>
2024-07-02 17:32:23 -0500

    Changed board to be a 2d array

--- tic_tac_toe_game.rb
+++ tic_tac_toe_game.rb
@@ -3,7 +3,7 @@ class TicTacToe
   attr_accessor :board, :current_move
 
   def initialize
-    @borad = []
+    @borad = [][]
     @current_move = ""
   end
 
@@ -24,6 +24,4 @@ class TicTacToe
 
   def play_game
   end
-
 end
-
