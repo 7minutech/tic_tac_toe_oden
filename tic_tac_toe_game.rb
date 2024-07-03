d921ce0b69db963bb4f6bfc27a773b00a5dc1bb2
7minutech <elijohnson1107@gmail.com>
2024-07-03 01:35:07 -0500

    Added some function to play game

--- tic_tac_toe_game.rb
+++ tic_tac_toe_game.rb
@@ -1,6 +1,7 @@
 # class that sets up a game of tic tac toe
 class TicTacToe
-  attr_accessor :board, :current_move, :x_o, :simple_board, :player_one_win, :player_two_win, :move_combinaitons
+  attr_accessor :board, :current_move, :x_o, :simple_board, :player_one_win,
+                :player_two_win, :move_combinaitons, :game_over, :rounds_played
 
   POSSIBLE_MOVES = (1..3).freeze
   WINNING_COMBINAITONS = (0..7).freeze
@@ -18,6 +19,7 @@ class TicTacToe
     @player_one_win = false
     @player_two_win = false
     @move_combinaitons = ""
+    @game_over = false
   end
 
   def fill_board
@@ -25,7 +27,7 @@ class TicTacToe
       BOARD_SPACES.each do |j|
         board[i][j] = "|" if j.odd?
         board[i][j] = "-" if i.odd?
-        board[i][j] = "x" if i.even? && j.even?
+        board[i][j] = " " if i.even? && j.even?
       end
     end
   end
@@ -53,7 +55,10 @@ class TicTacToe
 
   def valid_move?
     current_move.each do |num|
-      return false unless POSSIBLE_MOVES.include?(num)
+      unless POSSIBLE_MOVES.include?(num)
+        puts "Row and Column must be between [1,3]"
+        return false
+      end
     end
     true
   end
@@ -69,7 +74,7 @@ class TicTacToe
   def map_move
     row = current_move[0]
     col = current_move[1]
-    row -= 1
+    row = (row - 1) * 2
     col = (col - 1) * 2
     current_move[0] = row
     current_move[1] = col
@@ -87,48 +92,77 @@ class TicTacToe
     end
   end
 
-  def row_column_moves
-    col_arr = []
-    row_col_str = ""
+  def row_moves
     simple_board.each do |row|
-      row.each { |col| col_arr.push(col) }
-      row_col_str += "#{col_arr.reduce { |result, space| result + space }},"
-      row_col_str += "#{row.reduce { |result, space| result + space }},"
-      col_arr.clear
+      self.move_combinaitons += "#{row.reduce { |result, space| result + space }},"
     end
-    self.move_combinaitons = row_col_str
   end
 
   def diaganol_moves
     SIMPLE_BOARD_SPACES.each do |i|
       self.move_combinaitons += simple_board[i][i]
     end
+    self.move_combinaitons += ","
     counter = 2
     SIMPLE_BOARD_SPACES.each do |i|
       self.move_combinaitons += simple_board[i][i]
       counter -= 1
     end
+    self.move_combinaitons += ","
+
   end
 
   def update_move_combinations
-    row_column_moves
+    self.move_combinaitons = ""
+    row_moves
     diaganol_moves
+    puts move_combinaitons
   end
 
   def winner?
     if move_combinaitons.include?(PLAYER_ONE_WIN)
+      puts "Player one wins!"
       self.player_one_win = true
     elsif move_combinaitons.include?(PLAYER_TWO_WIN)
+      puts "Player two wins!"
       self.player_two_win = true
     else
       false
     end
   end
 
-  def play_game; end
+  def next_round
+    self.rounds_played += 1
+  end
+
+  def reset
+    @player_one_win = false
+    @player_two_win = false
+    @move_combinaitons = ""
+    @game_over = false
+    @current_move = " "
+    @rounds_played = 0
+    @x_o = " "
+  end
+
+  def play_game
+    fill_board
+    display_board
+    until game_over == true
+      player_move
+      convert_move
+      next unless valid_move?
+
+      map_move
+      x_o_selector
+      next_round
+      place_move
+      display_board
+      map_simple_board
+      update_move_combinations
+      self.game_over = true if winner?
+    end
+  end
 end
 game1 = TicTacToe.new
-game1.fill_board
-game1.display_board
-game1.map_simple_board
-game1.update_move_combinations
+game1.play_game
