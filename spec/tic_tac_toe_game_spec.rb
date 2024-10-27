f4bbadda587af735b1b7e669470415f2e6d7fc7a
7minutech <elijohnson1107@gmail.com>
2024-10-27 00:35:48 -0500

    feat: added tests for game_over? method

--- spec/tic_tac_toe_game_spec.rb
+++ spec/tic_tac_toe_game_spec.rb
@@ -1,10 +1,38 @@
-require_relative "../lib"
+require_relative "../lib/tic_tac_toe_game"
+require "pry-byebug"
 describe TicTacToe do
-  describe "#method" do
-    context "" do
-      subject(:game) { described_class.new }
-      it "returns" do
-      end
+  before do
+    allow($stdout).to receive(:write) # Suppresses all console output
+  end
+  let(:game) { described_class.new }
+  describe "#game_over" do
+    it "returns true if player 1 has a winning combination" do
+      allow(game).to receive(:gets).and_return("1,1", "2,1", "2,2", "3,1", "3,3")
+      game.play_game
+      player_1_win = game.send(:game_over?)
+      expect(player_1_win).to eq(true)
+    end
+    it "returns true if player 2 has a winning combination" do
+      allow(game).to receive(:gets).and_return("2,3", "1,1", "2,2", "2,1", "3,3", "3,1")
+      game.play_game
+      player_2_win = game.send(:game_over?)
+      expect(player_2_win).to eq(true)
+    end
+    it "returns true if no one wins by the 9th round" do
+      tie_moves = %w[1 2 3]
+      tie_moves = tie_moves.repeated_permutation(2).to_a.map! { |move| move.join(",") }
+      tie_moves.push(tie_moves[0])
+      tie_moves.shift
+      allow(game).to receive(:gets).and_return(*tie_moves)
+      game.play_game
+      tie_game = game.send(:game_over?)
+      expect(tie_game).to eq(true)
+    end
+    it "returns false when no one has won yet" do
+      allow(game).to receive(:gets).and_return("2,1", "3,2", "1,2")
+      3.times { game.send(:play_round) }
+      active_game = game.send(:game_over?)
+      expect(active_game).to eq(false)
     end
   end
 end
